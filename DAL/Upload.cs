d01c52aa9a55ed3d7a3f692b4d167b79d72e7f49
zouxiaozx <851230356@QQ.com>
2019-09-16 15:07:48 +0800

    Lx04

---
+++ DAL/Upload.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+namespace DAL
+{
+   public class Upload
+    {
+        private Upload() { }
+            private static Upload _instace = new Upload();
+        public static Upload Instance
+        {
+            get
+            {
+                return _instace;
+            }
+        }
+        public string UpImg(IFormFile file, string filename) {
+
+            if (file == null || !file.ContentType.StartsWith("iamge"))
+                return null;
+            else {
+                string ext = Path.GetExtension(file.FileName);
+                filename = $"{filename}{ext}";
+                using (FileStream fs = File.Create(filename))
+                {
+                    file.CopyTo(fs);
+                    fs.Flush();
+                }
+                return ext;
+            }
+        }
+        
+    }
+}
---
+++ Model/Model.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Model
+{
+    public class UserInfo
+    {
+        public string userName { get; set; }
+        public string passWord { get; set; }
+        public string qq { get; set; }
+        public string email { get; set; }
+        public string type { get; set; }
+        public string userImg { get; set; }
+    }
+    public class UserInfoNo : UserInfo
+    {
+        public int num { get; set; }
+    }
+    public class Page
+    {
+        public int pageIndex { get; set; }
+        public int pageSize { get; set; }
+    }
+    public class Activity
+    {
+        public int activityId { get; set; }
+        public string activityName { get; set; }
+        public DateTime endTime { get; set; }
+    }
+    public class WorkInfo
+    {
+        public int workId { get; set; }
+    }
+    public class Comment
+    {
+        public int commentId { get; set; }
+    }
+    public class WorkFindPage : Page
+    {
+        public string workName { get; set; }
+    }
+    public class WorkMyPage : Page
+    {
+        public string userNameP { get; set; }
+    }
+    public class CommentPage : Page
+    {
+        public string workId { get; set; }
+    }
+    public class ActivityNo : Activity {
+        public int num { get; set; }
+    }
+    public class ActivityName
+    {
+        public int activityId { get; set; }
+        public string activityName { get; set; }
+    }
+    public class WorkInfoNo : WorkInfo {
+        public int num { get; set; }
+        public string activityname { get; set; }
+    }
+    public class CommentNo : Comment {
+        public int num { get; set; }
+        public string workName { get; set; }
+    }
+}
